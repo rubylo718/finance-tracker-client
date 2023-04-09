21d219727648d569752ddbdd5049b0eeaf18d1c7
rubylo718 <rubylo718@gmail.com>
2023-04-09 23:12:51 +0800

    chore: delete useless default files and add bootstrap

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { useEffect } from 'react'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/customBootstrap.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	useEffect(() => {
+		import('bootstrap/dist/js/bootstrap.js')
+	}, [])
+	return <Component {...pageProps} />
 }
