32098d3ba9c2f9f4f47a9d393120ee1e7b02df40
rubylo718 <rubylo718@gmail.com>
2023-04-15 20:38:06 +0800

    feat: add logout function on nav component

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -1,17 +1,30 @@
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import Toast from '@/utils/toast-helper'
 
 const NavComponent = () => {
+	const router = useRouter()
+	const handleLogout = () => {
+		localStorage.removeItem('authToken')
+		Toast.fire({
+			icon: 'success',
+			title: 'Logout successfully. See U :)',
+		})
+		router.push('/users/login')
+	}
+
 	return (
 		<div className="container">
-			<nav
-				className="navbar fixed-top"
-				style={{ backgroundColor: '#e3f2fd' }}
-			>
+			<nav className="navbar fixed-top" style={{ backgroundColor: '#e3f2fd' }}>
 				<div className="container-fluid">
 					<Link className="navbar-brand" href="/">
 						Finance Tracker
 					</Link>
-					<button className="btn btn-light" role="button">
+					<button
+						onClick={handleLogout}
+						className="btn btn-light"
+						role="button"
+					>
 						Logout
 					</button>
 				</div>
